004f4622650487c4c8e7b4f4d011752ec61a7aed
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-06-29 19:55:38 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -87,8 +87,8 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                         List.generate(random_data.randomInteger(5, 10),
                                 (index) => random_data.randomName(true, false))
                             .toList(),
-                        List.generate(random_data.randomInteger(5, 10),
-                                (index) => random_data.randomInteger(10, 1000))
+                        List.generate(random_data.randomInteger(5, 5),
+                                (index) => random_data.randomInteger(1, 10))
                             .toList(),
                         List.generate(random_data.randomInteger(10, 20),
                                 (index) => random_data.randomDouble(1.0, 120.0))
